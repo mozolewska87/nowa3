80a597f920b1da31b946f7916baf83f4d797d2b9
Your Name <your.email@example.com>
2024-07-19 12:32:27 +0200

    odp

--- js.js
+++ js.js
@@ -1,1 +1,17 @@
 
+
+
+async function nowa(){
+    try{
+        const response=await fetch('http://api.nbp.pl/api/cenyzlota');
+        if(!response.ok){
+            throw new Error(response.statusText)
+        }
+        const data= response.json();
+        console.log(data);
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+nowa();
